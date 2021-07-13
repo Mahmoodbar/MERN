03ef7e4ad23efbd9cb3cc770e0757d568efd363c
mahmood Barakat <mahmood.barakat.2011@gmail.com>
2021-07-13 14:09:55 +0300

    mern

---
+++ react/react/firstjsx/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello Dojo</h1>
+      <h2>things i need to do </h2>
+      <p>learn react</p>
+      <p> climb mt everest</p>
+      <p> run a marathon</p>
+
+    </div>
+  );
+}
+
+export default App;
