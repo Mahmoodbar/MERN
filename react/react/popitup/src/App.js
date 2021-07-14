68e1177b6e23ce3ab3c13d13d44b680121d74112
mahmood Barakat <mahmood.barakat.2011@gmail.com>
2021-07-14 15:14:03 +0300

    mern

--- react/react/popitup/src/App.js
+++ react/react/popitup/src/App.js
@@ -5,13 +5,12 @@ import Info from './components/Info.js';
 function App() {
   return (
     <div className="App">
-     
-   <Info name="Jane Doe" Age="45" color="black" /> 
+   <Info name="Jane Doe" Age={45} color="black" /> 
 
-  <Info  name="smith John" Age="88" color="brown" />
+  <Info  name="smith John" Age={88} color="brown" />
 
-   <Info  name="millard" Age="20" color="black" />
-   <Info name="maria " Age="30" color="brown" />
+   <Info  name="millard" Age={20} color="black" />
+   <Info name="maria " Age={30} color="brown" />
     </div>
   );
 }
--- react/react/popitup/src/components/Info.js
+++ react/react/popitup/src/components/Info.js
@@ -1,17 +1,28 @@
 import React, { Component } from 'react';
 
 class Info extends Component {
-   
+    constructor(props) {
+        super(props);
+        this.state = {
+        Age: props.Age
+        };
+    }
+    addAge = () => {this.setState({Age:this.state.Age+1})}
 
-    render() {
+    render(){
+  
+
         return (
 
-            <div>
+            <div >
                 <h1> Name: {this.props.name}</h1>
-                <p> Age: {this.props.Age}</p>
+                <p> Age: {this.state.Age}</p>
                 <p> Hair Color:{this.props.color} </p>
+                <button onClick={ this.addAge }>Birthday button for {this.props.name}</button>
+                <br></br>
+                <br></br>
             </div>
-            )
+            );
         }
 }  
 export default Info;
