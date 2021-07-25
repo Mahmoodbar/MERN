b94db2fa8831461f36996e1fbc7dc1921c14fe3b
mahmood Barakat <mahmood.barakat.2011@gmail.com>
2021-07-25 10:19:26 +0300

    mern

---
+++ react/react/box/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import BoxGenerator from './component/BoxGenerator'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <BoxGenerator/>
+    </div>
+  );
+}
+
+export default App;
